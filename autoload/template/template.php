ea00a1f284b656c4b3c92e97c77f812767fb239c
thekaway404 <pikesob123123@gmail.com>
2019-01-10 16:27:13 -0200

    Template

--- autoload/template/template.php
+++ autoload/template/template.php
@@ -1,4 +1,13 @@
 <?php
+
+//Toda vez que você criar uma GET, tipo http://localhost/?profile=1, e também criar uma pasta no /autoload/template/page e coloque uma index.php
+// Você terá que colocar
+// Exemplo:
+// if(isset($_GET['profile'])){
+// 	$page = 'profile';
+// 	$idperfil = $_GET['profile'];
+// }
+
 // Variavel Página, serve para buscar no template // 
 $page = null;
 // Verifica se tem a variavel global //
